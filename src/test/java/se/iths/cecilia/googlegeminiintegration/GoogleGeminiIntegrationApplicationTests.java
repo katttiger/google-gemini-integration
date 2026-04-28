e747980bce4beaf4f160e84182dd0b016c5f634e
katttiger <katttiger@gmail.com>
2026-04-28 15:00:50 +0200

    updated GoogleGeminiIntegrationApplicationTests to pass CI/CD

--- src/test/java/se/iths/cecilia/googlegeminiintegration/GoogleGeminiIntegrationApplicationTests.java
+++ src/test/java/se/iths/cecilia/googlegeminiintegration/GoogleGeminiIntegrationApplicationTests.java
@@ -2,8 +2,11 @@ package se.iths.cecilia.googlegeminiintegration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import se.iths.cecilia.googlegeminiintegration.configuration.GeminiConfig;
 
 @SpringBootTest
+@Import(GeminiConfig.class)
 class GoogleGeminiIntegrationApplicationTests {
 
     @Test
