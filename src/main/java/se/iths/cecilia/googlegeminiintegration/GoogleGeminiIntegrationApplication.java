1c23fc93cf8e127d6ed42c7e611f049bb0e9ccee
katttiger <katttiger@gmail.com>
2026-04-28 10:50:17 +0200

    Initial commit

---
+++ src/main/java/se/iths/cecilia/googlegeminiintegration/GoogleGeminiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.googlegeminiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleGeminiIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleGeminiIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/cecilia/googlegeminiintegration/GoogleGeminiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.googlegeminiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleGeminiIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
