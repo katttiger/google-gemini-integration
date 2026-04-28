87fc00e9f786e6f3d93736a58352b730f2e31849
katttiger <katttiger@gmail.com>
2026-04-28 15:05:06 +0200

    updated GoogleGeminiIntegrationApplicationTests to pass CI/CD

--- src/main/java/se/iths/cecilia/googlegeminiintegration/configuration/GeminiConfig.java
+++ src/main/java/se/iths/cecilia/googlegeminiintegration/configuration/GeminiConfig.java
@@ -1,6 +1,7 @@
 package se.iths.cecilia.googlegeminiintegration.configuration;
 
 import com.google.genai.Client;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class GeminiConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "gemini.enabled", havingValue = "true", matchIfMissing = true)
     public Client client() {
         return new Client();
     }
